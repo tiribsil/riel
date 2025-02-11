bd048300c60311289ae1babcf42326e5c98c70f2
nowon <voidensatred@gmail.com>
2025-02-11 12:02:33 -0300

    base

--- utils.py
+++ utils.py
@@ -1,1 +1,23 @@
+class ExameLaboratorial:
+    def __init__(self, id, tipo, descricao):
+        self.__id = id
+        self._tipo = tipo
+        self._descricao = descricao
 
+    def get_id(self):
+        return self.__id
+
+    def __str__(self):
+        return f"Exame {self.__id} - Tipo: {self._tipo}, Descrição: {self._descricao}"
+
+class Paciente:
+    def __init__(self, __cpf, _nome, _data_nascimento):
+        self.cpf = __cpf
+        self.nome = _nome
+        self.data_nascimento = _data_nascimento
+
+    def get_cpf(self):
+        return self.__cpf
+
+    def __str__(self):
+        return f"Paciente {self.__cpf} - Nome: {self._nome}, Data de Nascimento: {self._data_nascimento}"
