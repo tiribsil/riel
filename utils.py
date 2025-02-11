516197a1b3990802ebcd717e8fbb1f79c3aa6163
nowon <voidensatred@gmail.com>
2025-02-11 12:06:08 -0300

    ops tinha feito besteira

--- utils.py
+++ utils.py
@@ -11,13 +11,14 @@ class ExameLaboratorial:
         return f"Exame {self.__id} - Tipo: {self._tipo}, Descrição: {self._descricao}"
 
 class Paciente:
-    def __init__(self, __cpf, _nome, _data_nascimento):
-        self.cpf = __cpf
-        self.nome = _nome
-        self.data_nascimento = _data_nascimento
+    def __init__(self, cpf, nome, data_nascimento):
+        self.__cpf = cpf
+        self._nome = nome
+        self._data_nascimento = data_nascimento
 
     def get_cpf(self):
         return self.__cpf
 
     def __str__(self):
         return f"Paciente {self.__cpf} - Nome: {self._nome}, Data de Nascimento: {self._data_nascimento}"
+
