20d0ca283afe7cffdccae8864f86d1220e2a16e3
nowon <voidensatred@gmail.com>
2025-02-11 21:55:46 -0300

    fila de espera

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import os
+import pickle
 import itertools
+from datetime import datetime
 
 class ExameLaboratorial:
     id_iter = itertools.count()
@@ -40,3 +43,37 @@ class RegistroExameLaboratorial:
                 f"  - {self.__exame}\n"
                 f"  - {self.__paciente}\n"
                 f"  - Data de Criação: {self.__data_criacao}")
+
+class FilaDeEspera:
+    def __init__(self):
+        self.__fila = []
+        if not os.path.exists('backup_fila.bak'): self.__salvar()
+        with open('backup_fila.bak', 'rb') as arquivo_backup:
+            self.__fila = pickle.load(arquivo_backup)
+
+    def adicionar(self, registro):
+        if not isinstance(registro, RegistroExameLaboratorial): return False
+        self.__fila.append(registro)
+        self.__salvar()
+        return True
+
+    def remover(self):
+        if not self.__fila: return None
+        registro_removido = self.__fila.pop(0)
+        self.__salvar()
+        return registro_removido
+
+    def limpar(self):
+        while self.__fila: self.remover()
+
+    def __salvar(self):
+        with open('backup_fila.bak', 'wb') as arquivo_backup:
+            pickle.dump(self.__fila, arquivo_backup)
+
+    def __str__(self):
+        if not self.__fila:
+            return "Fila de espera vazia."
+        return "\n".join(str(registro) for registro in self.__fila)
+
+    def __len__(self):
+        return len(self.__fila)
