08702e886b7277ffd2126fd49cb86057c5bbbe52
nowon <voidensatred@gmail.com>
2025-02-13 19:23:11 -0300

    todas as US(?)

--- app.py
+++ app.py
@@ -13,36 +13,31 @@ def index():
 @app.route('/registrar_exame', methods=['GET', 'POST'])
 def registrar_exame():
     if request.method == 'POST':
-        if registrador.registrar(int(request.form['id_exame']), request.form['cpf']): return render_template('inserido.html')
-        else: return render_template('cpf_invalido.html')
+        if registrador.registrar(int(request.form['id_exame']), request.form['cpf']):
+            return render_template('inserido.html')
+        else:
+            return render_template('cpf_invalido.html')
 
     return render_template('registrar_exame.html', exames=exames, pacientes=pacientes)
 
 
 @app.route('/chamar_paciente')
-# def chamar_paciente():
-#     registro_removido = fila_de_espera.remover()
-#
-#     if registro_removido:
-#         exames_em_coleta.inserir(registro_removido)
-#         mensagem = f"Paciente {registro_removido.cpf_paciente} chamado para o exame {registro_removido.id_exame}."
-#     else:
-#         mensagem = "Não há pacientes na fila de espera."
-#
-#     return render_template('chamar_paciente.html', mensagem=mensagem)
+def chamar_paciente():
+    registro_chamado = registrador.chamar_paciente()
+    if registro_chamado:
+        mensagem = f'Paciente {registro_chamado.get_paciente().get_cpf()} chamado.'
+    else:
+        mensagem = 'Fila de espera vazia!'
+
+    return render_template('chamar_paciente.html', mensagem=mensagem)
 
 
-@app.route('/dar_alta')
-# def dar_alta():
-    # registro_removido = exames_em_coleta.remover()
-    #
-    # if registro_removido:
-    #     exames_coletados.inserir(registro_removido)
-    #     mensagem = f"Exame {registro_removido.id_exame} do paciente {registro_removido.cpf_paciente} finalizado."
-    # else:
-    #     mensagem = "Não há exames em coleta."
-    #
-    # return render_template('dar_alta.html', mensagem=mensagem)
+@app.route('/dar_alta', methods=['GET', 'POST'])
+def dar_alta():
+    if request.method == 'POST':
+        registrador.dar_alta(request.form['cpf'])
+
+    return render_template('dar_alta.html')
 
 
 @app.route('/quadro_geral')
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pickle
 import itertools
 from datetime import datetime
 
+from tenacity import retry_if_exception_message
+
+
 class ExameLaboratorial:
     id_iter = itertools.count()
 
@@ -50,6 +53,12 @@ class RegistroExameLaboratorial:
         self.__paciente = paciente
         self.__data_criacao = data_criacao
 
+    def get_paciente(self):
+        return self.__paciente
+
+    def get_exame(self):
+        return self.__exame
+
     def __str__(self):
         return (f"Registro de Exame Laboratorial {self.__id}:\n"
                 f"  - {self.__exame}\n"
@@ -90,6 +99,77 @@ class FilaDeEspera:
     def __len__(self):
         return len(self.__fila)
 
+class ExamesEmColeta:
+    def __init__(self):
+        self.__lista = []
+        if not os.path.exists('backup_lista.bak'): self.__salvar()
+        with open('backup_lista.bak', 'rb') as arquivo_backup:
+            self.__lista = pickle.load(arquivo_backup)
+
+    def __salvar(self):
+        with open('backup_lista.bak', 'wb') as arquivo_backup:
+            pickle.dump(self.__lista, arquivo_backup)
+
+    def inserir(self, registro):
+        if not isinstance(registro, RegistroExameLaboratorial): return False
+        self.__lista.append(registro)
+        self.__salvar()
+        return True
+
+    def remover(self, cpf):
+        if not self.__lista: return None
+
+        indice = -1
+        for i, registro in enumerate(self.__lista):
+            paciente = registro.get_paciente()
+            if paciente.get_cpf() == cpf:
+                indice = i
+                break
+
+        if indice == -1: return False
+        registro_removido = self.__lista.pop(indice)
+        self.__salvar()
+        return registro_removido
+
+    def limpar(self):
+        while self.__lista: self.__lista.pop(0)
+        self.__salvar()
+
+    def __str__(self):
+        if not self.__lista:
+            return "Nenhum exame em coleta."
+        return "\n".join(str(registro) for registro in self.__lista)
+
+    def __len__(self):
+        return len(self.__lista)
+
+class ExamesColetados:
+    def __init__(self):
+        self.__arquivo_historico = 'arquivo_historico.hist'
+
+    def inserir(self, registro):
+        if not isinstance(registro, RegistroExameLaboratorial): return False
+        with open(self.__arquivo_historico, 'a') as arquivo:
+            arquivo.write(str(registro))
+            arquivo.close()
+        return True
+
+    def limpar(self):
+        with open(self.__arquivo_historico, 'w'):
+            pass
+
+    def __str__(self):
+        if not os.path.exists(self.__arquivo_historico):
+            return "Nenhum exame registrado."
+
+        with open(self.__arquivo_historico, 'r+') as arquivo:
+            conteudo = arquivo.read()
+            if len(conteudo) == 0:
+                arquivo.close()
+                return "Nenhum exame registrado."
+            else:
+                arquivo.close()
+                return conteudo
 
 exames = [
     ExameLaboratorial("Hemograma", "Análise de células sanguíneas"),
@@ -130,36 +210,42 @@ pacientes = [
 class Registrador:
     def __init__(self):
         self.__fila = FilaDeEspera()
-        # __em_coleta = ExamesEmColeta()
-        # __coletados = ExamesColetados()
+        self.__em_coleta = ExamesEmColeta()
+        self.__coletados = ExamesColetados()
 
     def limpar(self):
         self.__fila.limpar()
-        #self.__em_coleta.limpar()
-        #self.__coletados.limpar()
+        self.__em_coleta.limpar()
+        self.__coletados.limpar()
 
     def registrar(self, id_exame, cpf_paciente):
         try: paciente = next(p for p in pacientes if p.get_cpf() == cpf_paciente)
         except StopIteration: return False
-        print(f"Debug: Searching for exame with id_exame = {id_exame}")  # Print the ID being searched
-        print("Debug: Available exame IDs:")  # Print all available exame IDs
-        for e in exames:
-            print(f"Exame ID: {e.get_id()}")
         exame = next(e for e in exames if e.get_id() == id_exame)
 
         registro = RegistroExameLaboratorial(exame, paciente, datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
         self.__fila.inserir(registro)
         return True
 
+    def chamar_paciente(self):
+        registro_chamado = self.__fila.remover()
+        if self.__em_coleta.inserir(registro_chamado): return registro_chamado
+        else: return False
+
+    def dar_alta(self, cpf):
+        registro_alta = self.__em_coleta.remover(cpf)
+        if self.__coletados.inserir(registro_alta): return registro_alta
+        else: return False
+
     def exibir_fila_de_espera(self):
         return str(self.__fila)
 
     def exibir_exames_coletados(self):
-        return 'str(self.__coletados)'
+        return str(self.__coletados)
 
     def exibir_exames_em_coleta(self):
-        return 'str(self.__em_coleta)'
+        return str(self.__em_coleta)
 
     def exibir_quadro_geral(self):
-        return 'Fila de Espera\n' + str(self.__fila) + '\nExames em Coleta\n' + 'str(self.__coletados)' + '\nExames Coletados\n' + 'str(self.__em_coleta)'
+        return 'Fila de Espera\n' + str(self.__fila) + '\n\nExames em Coleta\n' + str(self.__em_coleta) + '\n\nExames Coletados\n' + str(self.__coletados)
 
